55465b86b5ea058e030afd9dd7c19c6998452148
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-24 17:54:39 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Parent from "./Parent";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+   return (
+      <div>
+         <Parent />
+      </div>
+   );
+};
 
-export default App
+export default App;
